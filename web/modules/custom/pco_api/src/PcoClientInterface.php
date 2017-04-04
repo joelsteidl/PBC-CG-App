d8b6a470a1bc95ca97d2aeae5fac232e37fc31fe
Joel Steidl <joelsteidl@gmail.com>
2017-04-04 14:11:19 -0600

    Updating param documenation

--- web/modules/custom/pco_api/src/PcoClientInterface.php
+++ web/modules/custom/pco_api/src/PcoClientInterface.php
@@ -5,15 +5,20 @@ namespace Drupal\pco_api;
 interface PcoClientInterface {
 
   /**
-   * Simple get request
-   * https://github.com/findbrok/php-watson-api-bridge
-   *
-   * @param $uri
-   *   A URI from the Watson Explorer https://watson-api-explorer.mybluemix.net/
-   * @param $query_params
-   *   Array of options applicable to the Watson API URI being called.
+   * Utilizes Drupal's httpClient to connect to Planning Center Online CRM
+   * Info: https://planning.center/
+   * API Docs: http://planningcenter.github.io/api-docs/
+   * 
+   * @param string $method
+   *   get, post, patch, delete, etc. See Guzzle documentation.
+   * @param string $endpoint
+   *   The PCO API endpoint (ex. people/v2/people)
+   * @param array $query
+   *   Query string parameters the endpoint allows (ex. ['per_page' => 50]
+   * @param array $body (converted to JSON)
+   *   Utilized for some endpoints
    * @return object
-   *   \GuzzleHttp\Psr7\Response
+   *   \GuzzleHttp\Psr7\Response body
    */
    public function connect($method, $endpoint, $query, $body);
 }

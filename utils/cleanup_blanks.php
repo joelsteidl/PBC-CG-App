a9ecc56ff89d33c65bf3d3944fd350629a0866d0
Joel Steidl <joelsteidl@gmail.com>
2022-07-05 22:28:53 -0600

    Updates to the latest version of Drupal

--- utils/cleanup_blanks.php
+++ utils/cleanup_blanks.php
@@ -3,7 +3,8 @@
 $storage = Drupal::getContainer()->get('entity_type.manager')->getStorage('node');
 $individuals = $storage->getQuery()
   ->condition('type', 'individual')
-  ->condition('title', '', '=')
+  ->condition('field_planning_center_id', NULL, 'IS NULL')
+  ->condition('field_pco_updated', NULL, 'IS NULL')
   ->accessCheck(FALSE)
   ->execute();
 
